95f4c6c955c6ea9bc569d94567becefae17ad184
capslock321 <rzeng1522@gmail.com>
2021-10-20 18:05:39 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="mcprot",
+    version="1.2.0",
+    author="capslock321",
+    description="Another Minecraft packet protocol handler, designed because quarry wasn't working for me.",
+    url="https://github.com/capslock321/mcprot",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
